270126f0d3c65e6fc17e558eddb5f71c081c05bf
sarveshlohar <48479171+sarveshlohar@users.noreply.github.com>
2020-10-19 11:51:35 +0530

    Create arrayrepresentations.cpp

---
+++ Array/arrayrepresentations.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int arr[10]={1,2,3,4,5,6,7,8,9,10};
+  cout<<*(arr+1)<<endl;
+  cout<<1[arr]<<endl;
+  cout<<arr[1]<<endl;
+  return 0;
+}
+// in this way we can use array 
